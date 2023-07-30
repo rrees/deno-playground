9614039267454aedcf1abc53cce95d6b8568330f
Robert Rees <shudderfix@gmail.com>
2023-07-30 22:49:32 +0100

    Accounts for draws and starts to build up the round function so it can be used in a shell

--- dq_fight.ts
+++ dq_fight.ts
@@ -7,27 +7,51 @@ function d6() {
 }
 
 interface Combatant {
+	speed: number,
 	brawn: number,
+	magic: number,
 	armour: number,
+	health: number,
 }
+
+type CombatResult = string | undefined;
 
 export function determineDamage(winner: Combatant, loser: Combatant, damageRoll: number): number {
 	return winner.brawn - loser.armour + damageRoll;
 }
 
+export function determineRoundWinner(hero: Combatant, heroRoll: number,
+	opponent: Combatant, opponentRoll: number): CombatResult {
+	const heroAttack = heroRoll + hero.speed;
+	const enemyAttack = opponentRoll + opponent.speed;
 
-function fightRound(data: any, diceRoller = d6) {
-	const heroAttack = diceRoller() + data.hero.brawn;
-	const enemyAttack = diceRoller() + data.enemy.brawn;
+	if (enemyAttack === heroAttack) {
+		return undefined;
+	}
 
-	const victor = heroAttack > enemyAttack ? "hero" : "enemy";
-	const loser = victor === "hero" ? "hero" : "enemy";
+	return heroAttack > enemyAttack ? "hero" : "enemy";
+}
+
+
+export function fightRound(data: any, diceRoller = d6) {
+	const heroAttack = diceRoller() + data.hero.speed;
+	const enemyAttack = diceRoller() + data.enemy.speed;
+
+	const victor = determineRoundWinner(data.hero, diceRoller(), data.enemy, diceRoller());
+
+	if (victor === undefined) {
+		console.log("Round is a tie, no damage done");
+		return;
+	}
+	const [winner, loser] = victor === "hero" ? [data.hero, data.enemy] : [data.enemy, data.hero];
 
 	const baseDamage = diceRoller();
 
-	console.log(data[victor]);
+	const damage = determineDamage(winner, loser, baseDamage)
 
-	console.log(`Winner: ${victor}, damage: ${baseDamage}`);
+	console.log(victor);
+
+	console.log(`Winner: ${victor}, damage roll: ${baseDamage}, total damage: ${damage}`);
 }
 
 if(import.meta.main) {
--- dq_fight_test.ts
+++ dq_fight_test.ts
@@ -1,18 +1,29 @@
 import * as asserts from "https://deno.land/std@0.192.0/testing/asserts.ts";
 
-import {determineDamage} from './dq_fight.ts';
+import {determineDamage, determineRoundWinner} from './dq_fight.ts';
 
 const defaultHero = {
 	brawn: 1,
 	armour: 1,
+	speed: 1,
+	health: 30,
+	magic: 0,
 };
 
 const defaultMonster = {
+	speed: 2,
 	brawn: 2,
 	armour: 2,
+	health: 15,
+	magic: 0,
 };
 
 Deno.test('Check calculate with brawn and armour', () => {
 	const result = determineDamage(defaultHero, defaultMonster, 3);
 	asserts.assertEquals(result, 2);
+});
+
+Deno.test('Check tied values in a combat have no winner', () => {
+	const result = determineRoundWinner(defaultHero, 3, defaultMonster, 2);
+	asserts.assertEquals(result, undefined);
 });

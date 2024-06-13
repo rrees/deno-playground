8989387b6e58a2d5972fc6ee63523fce96054b75
Robert Rees <shudderfix@gmail.com>
2024-06-13 08:51:25 +0100

    Creates a fully working implementation based on the remainder value rather than any complicated calculation

--- IdString.ts
+++ IdString.ts
@@ -1,10 +1,43 @@
 
 export class IdString {
+    static readonly #encodingLetters = "abcdefghijkmnpqrstuvwxyz"
+        + "123456789"
+        + "ABCDEFG";
+    
     static encode(n: number): string {
-        return n.toString();
+        const encodingModulus = this.#encodingLetters.length;
+        const firstCharacter = this.#encodingLetters.charAt(0);
+
+        let outputLetters : string[] = [];
+        let current = n;
+
+        while(current > 0) {
+            const currentChar = current % encodingModulus;
+            const divisor = Math.floor(current / encodingModulus);
+
+            current = divisor;
+
+            outputLetters.push(this.#encodingLetters.charAt(currentChar));
+        }
+
+        return outputLetters.join('');
+    
     }
 
     static decode(s: string): number {
-        return Number.parseInt(s);
+        const letters = s.split('');
+
+        let id = 0;
+        let exponent = 0;
+        const numberOfEncodingLetters = this.#encodingLetters.length;
+
+        for(const letter of letters) {
+
+            const value = this.#encodingLetters.indexOf(letter);
+            id += value * Math.pow(numberOfEncodingLetters, exponent);
+            exponent++;
+        }
+
+        return id;
     }
 }
--- id_string_test.ts
+++ id_string_test.ts
@@ -3,10 +3,32 @@ import { assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
 import { IdString } from './IdString.ts';
 
 Deno.test('Encoding and decoding are symmetrical', () => {
-    const exampleId = 10;
+    for (const exampleId of [10, 20, 40, 50, 75, 500, 1000, 10000]) {
 
-    assertEquals(
-        exampleId,
-        IdString.decode(IdString.encode(exampleId))
-    );
-}); 
+        assertEquals(
+            exampleId,
+            IdString.decode(IdString.encode(exampleId))
+        );
+    }
+});
+
+type TestEncodings = [number, string]
+
+Deno.test('encoding', () => {
+    for (const [id, targetString] of <TestEncodings[]>[
+        [2, 'c'],
+        [39, 'G'],
+        [40, 'ab'],
+        [44, 'eb'],
+        [45, 'fb']]) {
+        assertEquals(IdString.encode(id), targetString);
+    }
+});
+
+Deno.test('decoding', () => {
+    for (const [targetId, idString] of <TestEncodings[]>[
+        [2, 'c'],
+    ]) {
+        assertEquals(IdString.decode(idString), targetId);
+    }
+});
